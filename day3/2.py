4d468697570b75b4670d1941f3b5a13a24587ab1
Luca-x64 <luca.ghir04@gmail.com>
2023-12-04 13:30:28 +0100

    progress ex 3 pt.2

--- day3/2.py
+++ day3/2.py
@@ -44,16 +44,24 @@ for i in range(len(lines)):
                 aboveSub= lines[i-1][leftIndex:rightIndex-1]
             else:    
                 aboveSub= lines[i-1][leftIndex-1:rightIndex-1]
-
+        
         #Below
         if i+1 < len(lines):
             if leftIndex == 0:
                 belowSub = lines[i+1][leftIndex:rightIndex-1]
             else:
                 belowSub = lines[i+1][leftIndex-1:rightIndex-1]
+                print("c",leftIndex-1)
+                print("b",lines[i+1][:leftIndex-1])
+                print("a",re.findall('\d+',line[i+1][:leftIndex-1:-1]))
+            
+            side = [checkstr(aboveSub), checkstr(belowSub), checkstr(prima), checkstr(dopo)]
+
+
         print(prima,dopo,aboveSub,belowSub)
-        if checkstr(aboveSub) or checkstr(belowSub) or checkstr(prima) or checkstr(dopo):
-            sum += int(num)
+        if side.count(True) >=2 :
+            pass
+            #sum += int(num)
         
         startSub= rightIndex - 1
         
