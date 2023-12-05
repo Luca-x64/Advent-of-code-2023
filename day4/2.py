cc70426935aa6b3bcf12ed78430aba94239ce1ad
Luca-x64 <luca.ghir04@gmail.com>
2023-12-05 10:29:23 +0100

    done 4.2

--- day4/2.py
+++ day4/2.py
@@ -1,21 +1,25 @@
+import time
 lines = []
 with open("input.txt",'r') as f:
     lines = f.readlines()
 
-occurrencies = []
 ncards = len(lines)
 print(ncards)
+
 i=1
+a = time.now()
 for i in range(len(lines)):
-#while i < len(lines):
     line = lines[i][9:].replace("  ",",").replace(" ",",").replace("\n","")
     split = line.split(",|,")
     win = set(split[0].split(","))
     hand = set(split[1].split(","))
     cardsWon = len(win&hand)
-    occurrencies.append(cardsWon)
-    #for j in range(cardsWon):
-    #    lines.append(lines[i+j+1])
-    #i+=1
-
-print(ncards)
+    print(cardsWon)
+    for j in range(cardsWon):
+        for k in range(lines.count(lines[i])):
+            lines.append(lines[i+j+1])
+    i+=1
+    
+b = time.now()
+print(b-a)
+print(len(lines))
